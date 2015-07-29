4aabec80231551d94dfcf6d4d1121facad2ffe5f
Vasyl Shumeiko <vasyl.shumeiko@gmail.com>
2015-07-29 10:06:06 +0300

    Authentication module

--- src/auth/auth_module.js
+++ src/auth/auth_module.js
@@ -1,5 +1,45 @@
-var app = angular.module("SoftServeTesting", []);
+'use strict';
 
-app.controller("SoftServeTestingCtrl", function($scope) {
+//declare modules
 
-});
+angular.module('Authentication', []);
+angular.module('Admin', []);
+
+angular.module('SoftServeTesting', [
+	'Authentication',
+	'Admin',
+	'ngRoute',
+	'ngCookies'
+])
+	.config(['$routeProvider', function ($routeProvider) {
+
+		$routeProvider
+			.when('/', {
+				controller: 'LoginController',
+				templateUrl: 'auth.html',
+				hideMenus: true
+			})
+
+			.when('/admin', {
+				controller: 'AdminController',
+				templateUrl: '../admin/admin.html'
+			})
+
+			.otherwise({ redirectTo: '/' });
+	}])
+
+	.run(['$rootScope', '$location', '$cookieStore', '$http',
+		function ($rootScope, $location, $cookieStore, $http) {
+			// keep user logged in after page refresh
+			$rootScope.globals = $cookieStore.get('globals') || {};
+			if ($rootScope.globals.currentUser) {
+				$http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+			}
+
+			$rootScope.$on('$locationChangeStart', function (event, next, current) {
+				// redirect to login page if not logged in
+				if ($location.path() !== '/' && !$rootScope.globals.currentUser) {
+					$location.path('/');
+				}
+			});
+		}]);
