3f8ebf77a95b445fe42480329adccdc493e789cd
Serhiy91 <serhiyzi@mail.ru>
2015-07-30 21:01:46 +0300

    moved components folder

--- src/app.config.js
+++ src/app.config.js
@@ -1,1 +1,19 @@
-console.log("app.config");
+'use strict';
+
+var subjectCtrl = angular.module('subjectCtrl', []);
+subjectCtrl.controller('SubjectsListCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('http://dtapi.local/subject/getRecords').success(function (data) {
+        $scope.subjects = data;
+    });
+
+    $scope.sortField = undefined;
+    $scope.reverse = false;
+    $scope.sort = function (fieldName) {
+        if ($scope.sortField === fieldName) {
+            $scope.reverse = !$scope.reverse;
+        } else {
+            $scope.sortField = fieldName;
+            $scope.reverse = false;
+        }
+    }
+}]);
--- src/app.module.js
+++ src/app.module.js
@@ -1,8 +1,11 @@
 'use strict';
 
-/* Controllers */
+/* Module */
 
-var ssTest = angular.module('ssTest', ['ui.router']);
+var ssTest = angular.module('ssTest', [
+    'ui.router',
+    'subjectCtrl'
+]);
 
 ssTest.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
     $urlRouterProvider.otherwise('/');
@@ -10,22 +13,4 @@ ssTest.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvid
         url: '/',
         templateUrl: 'http://dtapi.local/~pupkin/components/subjects/view.html'
     })
-}]);
-
-ssTest.controller('SubjectsListCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('http://ec2-54-69-82-91.us-west-2.compute.amazonaws.com/subject/getRecords').success(function(data) {
-        $scope.subjects = data;
-    });
-
-    $scope.sortField = undefined;
-    $scope.reverse = false;
-
-    $scope.sort = function(fieldName) {
-        if ($scope.sortField === fieldName) {
-            $scope.reverse = !$scope.reverse;
-        } else {
-            $scope.sortField =fieldName;
-            $scope.reverse = false;
-        }
-    }
 }]);
