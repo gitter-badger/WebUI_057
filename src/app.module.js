86f4235e5562e2602add41dba0530c1d2fc9d62f
Serhiy91 <serhiyzi@mail.ru>
2015-07-30 13:29:42 +0300

    configured ui-router

--- src/app.module.js
+++ src/app.module.js
@@ -2,8 +2,30 @@
 
 /* Controllers */
 
-var ssTest = angular.module('ssTest', []);
+var ssTest = angular.module('ssTest', ['ui.router']);
 
-ssTest.controller('', ['$scope', function($scope) {
+ssTest.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('subjects', {
+        url: '/',
+        templateUrl: 'http://dtapi.local/~pupkin/components/subjects/view.html'
+    })
+}]);
 
-});
+ssTest.controller('SubjectsListCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('http://ec2-54-69-82-91.us-west-2.compute.amazonaws.com/subject/getRecords').success(function(data) {
+        $scope.subjects = data;
+    });
+
+    $scope.sortField = undefined;
+    $scope.reverse = false;
+
+    $scope.sort = function(fieldName) {
+        if ($scope.sortField === fieldName) {
+            $scope.reverse = !$scope.reverse;
+        } else {
+            $scope.sortField =fieldName;
+            $scope.reverse = false;
+        }
+    }
+}]);
