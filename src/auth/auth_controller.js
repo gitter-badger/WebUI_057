758824d01f4f21d40483a2200e4382e15faabc6b
Vasyl Shumeiko <vasyl.shumeiko@gmail.com>
2015-07-30 10:57:39 +0300

    fixing tooltips

--- src/auth/auth_controller.js
+++ src/auth/auth_controller.js
@@ -1,12 +1,14 @@
 auth.controller("AuthorizationCtrl", ["$scope", "$http", "$window",
 	function($scope, $http, $window) {
 		$scope.url = "/login/index";
+		$scope.dataLoading = false;
 
 		$scope.login = function() {
 			$scope.authData = JSON.stringify({
 				username: $scope.userName,
 				password: $scope.password
 			});
+			$scope.dataLoading = true;
 
 			$http.post($scope.url, $scope.authData)
 				.success(function(data, status) {
@@ -15,23 +17,35 @@ auth.controller("AuthorizationCtrl", ["$scope", "$http", "$window",
 					$scope.checkResponse(data);
 				})
 				.error(function(data, status) {
-					$scope.status = status || "Запит відхилено.";
+					$scope.status = status || "Р—Р°РїРёС‚ РІС–РґС…РёР»РµРЅРѕ.";
 					$scope.data = data;
 				});
 		};
 
 		$scope.checkResponse = function(data) {
 			if (data.response === "ok") {
+				$scope.removeAlarm();
 				$window.location.href = "../admin/admin.html";
 			} else {
-				generateSpanElem();
+				$scope.generateSpanElem();
+				$scope.dataLoading = false;
 			}
 		};
 
-		function generateSpanElem() {
+		$scope.generateSpanElem = function() {
+			$scope.removeAlarm();
+
 			var span = document.createElement("span");
-			span.innerHTML = "Невірний логін або пароль!";
+			span.innerHTML = "РќРµРІС–СЂРЅРёР№ Р»РѕРіС–РЅ Р°Р±Рѕ РїР°СЂРѕР»СЊ!";
 			span.classList.add('spanSubmit');
 			document.getElementById('submit').insertAdjacentElement("beforeEnd", span);
-		}
+		};
+
+		$scope.removeAlarm = function() {
+			var div = document.getElementById('submit'),
+				span = div.querySelector('.spanSubmit');
+			if (span) {
+				div.removeChild(span);
+			}
+		};
 }]);
